a53e0298fc99659d90adcee81f9b98ccc0872af0
Mateman <pcuyeu@gmail.com>
2018-05-30 09:26:05 -0300

    ultima version demo 1

--- app/config/ingenieriadev.sql
+++ app/config/ingenieriadev.sql
@@ -21,7 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ingenieriadev`
 --
-
+CREATE DATABASE ingenieriadev;
+use ingenieriadev
 -- --------------------------------------------------------
 
 --

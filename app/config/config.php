e7ea8dc96df215b608caaa654ab7c8869f910283
mateman <cuyeu@hotmail.com>
2019-09-02 09:01:31 -0300

    Toma la ip real en el momento de ejecucion y queda independiente de si cambia la ip en algun momento el server

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,50 @@
 <?php 
 
+function ObtenerIP()
+{
+    $ip = "";
+    if(isset($_SERVER))
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))
+        {
+            $ip=$_SERVER['HTTP_CLIENT_IP'];
+        }
+        elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+        {
+            $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        else
+        {
+            $ip=$_SERVER['REMOTE_ADDR'];
+        }
+    }
+    else
+    {
+        if ( getenv( 'HTTP_CLIENT_IP' ) )
+        {
+            $ip = getenv( 'HTTP_CLIENT_IP' );
+        }
+        elseif( getenv( 'HTTP_X_FORWARDED_FOR' ) )
+        {
+            $ip = getenv( 'HTTP_X_FORWARDED_FOR' );
+        }
+        else
+        {
+            $ip = getenv( 'REMOTE_ADDR' );
+        }
+    }
+    // En algunos casos muy raros la ip es devuelta repetida dos veces separada por coma
+    if(strstr($ip,','))
+    {
+        $ip = array_shift(explode(',',$ip));
+    }
+    return $ip;
+}
+
     define('DB_HOST','localhost');
     define('DB_USUARIO','root');
     define('DB_PASSWORD','');
     define('DB_NOMBRE','ingenieria3');
     define('RUTA_APP', dirname(dirname(__FILE__)));
-    define('RUTA_URL', 'http://localhost/Ing-Soft2');
+    define('RUTA_URL', 'http://'.ObtenerIP().'/Ing-Soft2');
     define('NOMBRE_SITIO',' _NOMBRE_SITIO');
